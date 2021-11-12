4285d1a34af4709d5c9955517b501194f5e1d422
Marina Robleño <marinarsanisidro@gmail.com>
2021-11-12 12:09:45 +0100

    Changed header color

--- src/App.js
+++ src/App.js
@@ -7,35 +7,48 @@ import {  Route, Link, Routes } from 'react-router-dom';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import { FaCameraRetro } from "react-icons/fa";
+import { IconContext } from "react-icons";
+import { createTheme, ThemeProvider } from '@mui/material/styles'
 
 function App() {
-
+  const theme = createTheme({
+    palette: {
+      neutral: {
+        main: '#ffffff',
+        contrastText: '#fff',
+      },
+    },
+  });
   return (
     <>
     <header>
-      <div className='header'>
-        <FaCameraRetro className='camera-icon'/>
-        <h2>PHOTO APP LOGO</h2>
-        <Box sx={{ flexGrow: 1 }}>
-          <Grid 
-            container spacing={1} 
-            direction="row"
-            justifyContent="flex-end"
-            alignItems="flex-start"
-            >
-            <Grid item xs='auto'>
-              <Link to='/search' style={{ textDecoration: 'none' }}>
-                <Button variant="contained" color='secondary' size='large' style={{ fontWeight: 'bold', margin: 10 }}>SEARCH</Button>
-              </Link>
+      <ThemeProvider theme={theme}>
+        <div className='header'>
+          <IconContext.Provider value={{ className: "camera-icon" }}>
+            <FaCameraRetro />
+          </IconContext.Provider>
+          <h3 className='app-title'>PHOTO APP LOGO</h3>
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid 
+              container spacing={1} 
+              direction="row"
+              justifyContent="flex-end"
+              alignItems="flex-start"
+              >
+              <Grid item xs='auto'>
+                <Link to='/search' style={{ textDecoration: 'none' }}>
+                  <Button variant="outlined" color='neutral' size='large' style={{ fontWeight: 'bold', margin: 10 }}>SEARCH</Button>
+                </Link>
+              </Grid>
+              <Grid item xs='auto'>
+                <Link to='/my-photos'style={{ textDecoration: 'none'}}>
+                  <Button variant="outlined" color='neutral' size='large' style={{ fontWeight: 'bold', margin: 10 }}>My Photos</Button>
+                </Link>
+              </Grid>
             </Grid>
-            <Grid item xs='auto'>
-              <Link to='/my-photos'style={{ textDecoration: 'none'}}>
-                <Button variant="outlined" color='secondary' size='large' style={{ fontWeight: 'bold', margin: 10 }}>My Photos</Button>
-              </Link>
-            </Grid>
-          </Grid>
-        </Box>
-      </div>
+          </Box>
+        </div>
+      </ThemeProvider>
     </header>
       <Routes>
         <Route path='/search' element={<Search />}/>       
