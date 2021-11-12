2ded8b618b7aef7b57915052704392a2902b9863
Marina Robleño <marinarsanisidro@gmail.com>
2021-11-12 11:41:20 +0100

    Css arrangements: preparing main header

--- src/App.js
+++ src/App.js
@@ -6,32 +6,36 @@ import { Search } from './components/Search.jsx'
 import {  Route, Link, Routes } from 'react-router-dom';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import { FaCameraRetro } from "react-icons/fa";
 
 function App() {
 
   return (
     <>
     <header>
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid 
-          container spacing={1} 
-          direction="row"
-          justifyContent="flex-end"
-          alignItems="flex-start"
-          >
-          <Grid item xs='auto'>
-            <Link to='/search' style={{ textDecoration: 'none' }}>
-              <Button variant="contained" color='secondary' size='large' style={{ fontWeight: 'bold', margin: 5 }}>PHOTO APP LOGO</Button>
-            </Link>
+      <div className='header'>
+        <FaCameraRetro className='camera-icon'/>
+        <h2>PHOTO APP LOGO</h2>
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid 
+            container spacing={1} 
+            direction="row"
+            justifyContent="flex-end"
+            alignItems="flex-start"
+            >
+            <Grid item xs='auto'>
+              <Link to='/search' style={{ textDecoration: 'none' }}>
+                <Button variant="contained" color='secondary' size='large' style={{ fontWeight: 'bold', margin: 10 }}>SEARCH</Button>
+              </Link>
+            </Grid>
+            <Grid item xs='auto'>
+              <Link to='/my-photos'style={{ textDecoration: 'none'}}>
+                <Button variant="outlined" color='secondary' size='large' style={{ fontWeight: 'bold', margin: 10 }}>My Photos</Button>
+              </Link>
+            </Grid>
           </Grid>
-          <Grid item xs='auto'>
-            <Link to='/my-photos'style={{ textDecoration: 'none'}}>
-              <Button variant="outlined" color='secondary' size='large' style={{ fontWeight: 'bold', margin: 5 }}>My Photos</Button>
-            </Link>
-          </Grid>
-        </Grid>
-      </Box>
-
+        </Box>
+      </div>
     </header>
       <Routes>
         <Route path='/search' element={<Search />}/>       
